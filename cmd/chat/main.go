1e6fe58945e377d6264cf150e5a8443ae2af024e
kanamatov2m <kanamatov2m@gmail.com>
2023-06-19 16:05:05 +0300

    feat: create logic for chat rooms

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -2,82 +2,248 @@ package main
 
 import (
 	"context"
+	"errors"
+	"io"
 	"log"
+	"math/rand"
+	"net"
 	"net/http"
-	"nhooyr.io/websocket"
+	"os"
+	"os/signal"
+	"sync"
 	"time"
+
+	"golang.org/x/time/rate"
+
+	"nhooyr.io/websocket"
 )
 
-type Server struct {
-	conns map[string]*websocket.Conn
+// chatServer enables broadcasting to a set of subscribers.
+type chatServer struct {
+	// subscriberMessageBuffer controls the max number
+	// of messages that can be queued for a subscriber
+	// before it is kicked.
+	//
+	// Defaults to 16.
+	subscriberMessageBuffer int
+
+	// publishLimiter controls the rate limit applied to the publish endpoint.
+	//
+	// Defaults to one publish every 100ms with a burst of 8.
+	publishLimiter *rate.Limiter
+
+	// logf controls where logs are sent.
+	// Defaults to log.Printf.
+	logf func(f string, v ...interface{})
+
+	// serveMux routes the various endpoints to the appropriate handler.
+	serveMux http.ServeMux
+
+	subscribersMu sync.Mutex
+	subscribers   map[string]map[*subscriber]bool
 }
 
-func NewServer() *Server {
-	return &Server{
-		conns: make(map[string]*websocket.Conn),
+// newChatServer constructs a chatServer with the defaults.
+func newChatServer() *chatServer {
+	cs := &chatServer{
+		subscriberMessageBuffer: 16,
+		logf:                    log.Printf,
+		subscribers:             make(map[string]map[*subscriber]bool),
+		publishLimiter:          rate.NewLimiter(rate.Every(time.Millisecond*100), 8),
 	}
+	cs.serveMux.Handle("/", http.FileServer(http.Dir("assets/.")))
+	cs.serveMux.HandleFunc("/subscribe", cs.subscribeHandler)
+	cs.serveMux.HandleFunc("/publish", cs.publishHandler)
+
+	return cs
 }
 
-func (s *Server) echoHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Star send")
-	chatIds, ok := r.URL.Query()["chatId"]
+// subscriber represents a subscriber.
+// Messages are sent on the msgs channel and if the client
+// cannot keep up with the messages, closeSlow is called.
+type subscriber struct {
+	id        int
+	msgs      chan []byte
+	closeSlow func()
+}
 
-	if !ok && len(chatIds) < 1 {
-		log.Println("Not found chat ID")
+func (cs *chatServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	cs.serveMux.ServeHTTP(w, r)
+}
+
+// subscribeHandler accepts the WebSocket connection and then subscribes
+// it to all future messages.
+func (cs *chatServer) subscribeHandler(w http.ResponseWriter, r *http.Request) {
+	chatId := r.URL.Query().Get("chatId")
+	if chatId == "" {
+		cs.logf("%v", "Get parameter is empty")
 		return
 	}
-	chatId := chatIds[0]
-	log.Println("chat id " + chatId)
-	conn, ok := s.conns[chatId]
-	if !ok {
-		log.Println("chat not found")
-		c, err := websocket.Accept(w, r, &websocket.AcceptOptions{
-			InsecureSkipVerify: true,
-		})
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		defer func(c *websocket.Conn, code websocket.StatusCode, reason string) {
-			err := c.Close(code, reason)
-			if err != nil {
-				log.Fatalln(err)
-			}
-		}(c, websocket.StatusInternalError, "the sky is falling")
-		s.conns[chatId] = c
-		conn = c
-	} else {
-
+	c, err := websocket.Accept(w, r, nil)
+	if err != nil {
+		cs.logf("%v", err)
+		return
 	}
-	log.Println("chat found")
+	defer c.Close(websocket.StatusInternalError, "")
+
+	err = cs.subscribe(r.Context(), c, chatId)
+	if errors.Is(err, context.Canceled) {
+		return
+	}
+	if websocket.CloseStatus(err) == websocket.StatusNormalClosure ||
+		websocket.CloseStatus(err) == websocket.StatusGoingAway {
+		return
+	}
+	if err != nil {
+		cs.logf("%v", err)
+		return
+	}
+}
+
+// publishHandler reads the request body with a limit of 8192 bytes and then publishes
+// the received message.
+func (cs *chatServer) publishHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	chatId := r.URL.Query().Get("chatId")
+	if chatId == "" {
+		cs.logf("%v", "Get parameter is empty")
+		return
+	}
+	body := http.MaxBytesReader(w, r.Body, 8192)
+	msg, err := io.ReadAll(body)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusRequestEntityTooLarge), http.StatusRequestEntityTooLarge)
+		return
+	}
+
+	cs.publish(msg, chatId)
+
+	w.WriteHeader(http.StatusAccepted)
+}
+
+// subscribe subscribes the given WebSocket to all broadcast messages.
+// It creates a subscriber with a buffered msgs chan to give some room to slower
+// connections and then registers the subscriber. It then listens for all messages
+// and writes them to the WebSocket. If the context is cancelled or
+// an error occurs, it returns and deletes the subscription.
+//
+// It uses CloseRead to keep reading from the connection to process control
+// messages and cancel the context if the connection drops.
+func (cs *chatServer) subscribe(ctx context.Context, c *websocket.Conn, chatId string) error {
+	ctx = c.CloseRead(ctx)
+
+	s := &subscriber{
+		id:   rand.Intn(1000),
+		msgs: make(chan []byte, cs.subscriberMessageBuffer),
+		closeSlow: func() {
+			c.Close(websocket.StatusPolicyViolation, "connection too slow to keep up with messages")
+		},
+	}
+	cs.addSubscriber(s, chatId)
+	defer cs.deleteSubscriber(s, chatId)
 
 	for {
-		log.Println("start loop")
-		ctx, cancel := context.WithTimeout(context.Background(), time.Minute*10)
-		defer cancel()
-
-		_, message, err := conn.Read(ctx)
-		log.Printf("Received error %v", err)
-		if err != nil {
-			break
+		select {
+		case msg := <-s.msgs:
+			cs.logf("message sent id %v", s.id)
+			err := writeTimeout(ctx, time.Second*5, c, msg)
+			if err != nil {
+				return err
+			}
+		case <-ctx.Done():
+			return ctx.Err()
 		}
-		log.Printf("Received %v", message)
-
-		err = conn.Write(ctx, websocket.MessageText, message)
-		log.Printf("Sended error %v", err)
-		if err != nil {
-			break
-		}
-		log.Printf("Sended %v", message)
 	}
+}
+
+// publish publishes the msg to all subscribers.
+// It never blocks and so messages to slow subscribers
+// are dropped.
+func (cs *chatServer) publish(msg []byte, chatId string) {
+	cs.subscribersMu.Lock()
+	defer cs.subscribersMu.Unlock()
+
+	err := cs.publishLimiter.Wait(context.Background())
+	if err != nil {
+		return
+	}
+
+	for s, _ := range cs.subscribers[chatId] {
+		select {
+		case s.msgs <- msg:
+		default:
+			go s.closeSlow()
+		}
+	}
+}
+
+// addSubscriber registers a subscriber.
+func (cs *chatServer) addSubscriber(s *subscriber, chatId string) {
+	cs.subscribersMu.Lock()
+	if _, found := cs.subscribers[chatId]; !found {
+		cs.subscribers[chatId] = make(map[*subscriber]bool)
+	}
+	cs.subscribers[chatId][s] = true
+	cs.subscribersMu.Unlock()
+}
+
+// deleteSubscriber deletes the given subscriber.
+func (cs *chatServer) deleteSubscriber(s *subscriber, chatId string) {
+	cs.subscribersMu.Lock()
+	delete(cs.subscribers[chatId], s)
+	cs.subscribersMu.Unlock()
+}
+
+func writeTimeout(ctx context.Context, timeout time.Duration, c *websocket.Conn, msg []byte) error {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	return c.Write(ctx, websocket.MessageText, msg)
 }
 
 func main() {
-	server := NewServer()
-	address := "localhost:1234"
-	http.HandleFunc("/echo", server.echoHandler)
-	log.Printf("Starting server, go to http://%s/ to try it out!", address)
-	http.Handle("/", http.FileServer(http.Dir("assets")))
-	err := http.ListenAndServe(address, nil)
-	log.Fatal(err)
+	log.SetFlags(0)
+
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// run initializes the chatServer and then
+// starts a http.Server for the passed in address.
+func run() error {
+	l, err := net.Listen("tcp", "localhost:1234")
+	if err != nil {
+		return err
+	}
+	log.Printf("listening on http://%v", l.Addr())
+
+	cs := newChatServer()
+	s := &http.Server{
+		Handler:      cs,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 10,
+	}
+	errc := make(chan error, 1)
+	go func() {
+		errc <- s.Serve(l)
+	}()
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt)
+	select {
+	case err := <-errc:
+		log.Printf("failed to serve: %v", err)
+	case sig := <-sigs:
+		log.Printf("terminating: %v", sig)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	return s.Shutdown(ctx)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module itdagestan/go-chat
 go 1.19
 
 require (
-	github.com/klauspost/compress v1.10.3 // indirect
-	nhooyr.io/websocket v1.8.7 // indirect
+	golang.org/x/time v0.3.0
+	nhooyr.io/websocket v1.8.7
 )
+
+require github.com/klauspost/compress v1.10.3 // indirect
